4167c38d471bab7b9fc3655794c3c53ec83ea6e4
Student User <student@workstation.lab.example.com>
2023-12-20 16:36:50 -0500

    Ajustado pagina inicial

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello World! - Aqui  o lugar para mudar o que est pedindo na proava\n');
 });
 
 app.listen(8080, function () {
